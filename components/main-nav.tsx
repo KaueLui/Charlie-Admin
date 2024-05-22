2c3832392de2b8049398a6c4d67990457c6d4ac5
Louiis04 <luiseduardosport1000@gmail.com>
2024-05-22 18:29:21 -0300

    Charlie-Settings-Page

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -13,10 +13,15 @@ export function MainNav({
 
     const routes = [
         {
+            href: `/${params.storeId}`,
+            label: 'Visão Geral',
+            active: pathname === `/${params.storeId}`,
+        },
+        {
             href: `/${params.storeId}/settings`,
             label: 'Settings',
             active: pathname === `/${params.storeId}/settings`,
-        }
+        },
     ];
 
     return (
