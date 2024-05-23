baa3e63cf481b083ad86118fe11e3fe895f48c05
Louiis04 <luiseduardosport1000@gmail.com>
2024-05-23 17:25:43 -0300

    Charlie-Fix-DataTables

--- app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
@@ -21,13 +21,14 @@ const BillboardsPage = async ({
     const formattedBillboards: BillboardColumn[] = billboards.map((item) => ({
         id: item.id,
         label: item.label,
-        createdAt: format(item.createdAt, "MMMM do, yyyy")
+        createdAt: format(item.createdAt, "MMMM do, yyyy"),
+        data: []
     }));
 
     return (
         <div className="flex-col">
            <div className="flex-1 space-y-4 p-8 pt-6">
-                <BillboardClient data={billboards} />
+                <BillboardClient data={formattedBillboards} />
            </div>
         </div>
     );
